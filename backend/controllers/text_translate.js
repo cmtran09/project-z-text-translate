fdd0c7a6b8b0853712575dc262b09c06941f948e
cmtran09 <cmtran@hotmail.co.uk>
2020-04-29 18:12:04 +0100

    able to translate text with hard coded langauge

--- backend/controllers/text_translate.js
+++ backend/controllers/text_translate.js
@@ -2,14 +2,15 @@ const axios = require('axios')
 
 function translateAndText(req, res) {
 	
-	// const key = 'trnsl.1.1.20191202T095947Z.54175132da211fc1.7560318e65d14be6fcc297876ea8f7cb07e89154'
-	// const text = req.body.message
+	const key = 'trnsl.1.1.20191202T095947Z.54175132da211fc1.7560318e65d14be6fcc297876ea8f7cb07e89154'
+	const text = req.body.message
 
-	console.log(req.body)
-
-
-	// axios.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=en-ru`)
-	// .then((res) => console.log(res))
+	// console.log('1st',req.body)
+	// console.log('2nd',text)
+	
+	axios.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=en-ru`)
+	.then((res) => console.log(res))
+	.catch(err=>console.log(err))
 }
 
 module.exports = {
