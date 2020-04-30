1fec41f121d21094894f0eafbf4c6de0b008c835
cmtran09 <cmtran@hotmail.co.uk>
2020-04-30 14:49:56 +0100

    translating from langague list

--- backend/controllers/text_translate.js
+++ backend/controllers/text_translate.js
@@ -4,8 +4,9 @@ function translateAndText(req, res) {
 	
 	const key = 'trnsl.1.1.20191202T095947Z.54175132da211fc1.7560318e65d14be6fcc297876ea8f7cb07e89154'
 	const text = req.body.message
+	const lang = req.body.language
 
-	axios.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=en-ru`)
+	axios.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=en-${lang}`)
 	.then((res) => console.log(res))
 	.catch(err=>console.log(err))
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,16 +10,17 @@ const App = () => {
 
   const [data, setData] = useState({
     message: "",
+    language: "",
     test: "beans"
   })
   const [languages, setLanguages] = useState([])
   const [error, setError] = useState()
 
-  useEffect(()=>{
+  useEffect(() => {
     const key = 'trnsl.1.1.20191202T095947Z.54175132da211fc1.7560318e65d14be6fcc297876ea8f7cb07e89154'
     axios.get(`https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=${key}`)
       .then(resp => setLanguages(resp.data.dirs))
-  },[])
+  }, [])
 
   function handleChange(e) {
     setData({ ...data, [e.target.name]: e.target.value })
@@ -34,13 +35,23 @@ const App = () => {
   }
 
 
-  const languagesList = languages.filter(language => language.startsWith('uk')) 
+  const languagesList = languages.filter(language => language.startsWith('uk'))
 
   return (
     <div className="">
       <h1>Hello mate</h1>
       <form onSubmit={e => handleSubmit(e)}>
-        <input name="message" onChange={handleChange} type="text" />
+        <input name="message" onChange={e=> handleChange(e)} type="text" />
+        <select name='language' onChange={e => handleChange(e)}>
+          <option>Select language</option>
+          {languagesList.map((elem, id) => {
+            return (
+              <option key={id} value={elem.slice(-2)} >
+                {elem.slice(-2)}
+              </option>
+            )
+          })}
+        </select>
         <button>submit</button>
       </form>
       <button onClick={e => console.log(data)}>log</button>
