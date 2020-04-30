5a5e2b61802f014c938a7e252da38b7d603e1a32
cmtran09 <cmtran@hotmail.co.uk>
2020-04-30 23:39:28 +0100

    problem has arisen

--- backend/controllers/text_translate.js
+++ backend/controllers/text_translate.js
@@ -2,27 +2,37 @@ const axios = require("axios")
 
 function translateAndText(req, res) {
 
-	const key = "trnsl.1.1.20191202T095947Z.54175132da211fc1.7560318e65d14be6fcc297876ea8f7cb07e89154"
+	const key = "trnsl.1.1.20200430T193714Z.38528beeb497af85.ccc8582dffab7fb59f5232f80c022fe83f128e44"
 	const text = req.body.message
 	const lang = req.body.language
+	const recipientNumber = req.body.number
 
-	const client = require("twilio")(accountSid, authToken)
 	const twilioNumber = "+13163134784"
 	const accountSid = "AC7ca5ae686cef0503c846ba87009871ae"
 	const authToken = "cd018fe2561d1653477db763def27dbf"
 
+	const client = require("twilio")(accountSid, authToken)
+
 	axios.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=en-${lang}`)
-		.then((res) => {
-			console.log(res)
+		.then(translatedText => {
+			console.log("translatedText.data : ", translatedText.data)
 			client.lookups.phoneNumbers(number)
 				.fetch({ countryCode: "GB" })
 				.then(phone_number => console.log(phone_number.phoneNumber))
 				.catch(err => res.status(400).json({ message: err }))
+
+			client.messages
+				.create({
+					body: translatedText.data.text[0],
+					from: twilioNumber,
+					to: `${recipientNumber}`
+				})
+				.then(message => {
+					console.log(message.sid)
+					if (message.sid) return res.status(200).json({ message: "Translated message sent!" })
+				})
 		})
-		.catch(err => {
-			console.log(err.response.data)
-			err => res.status(502).json({ message: err })
-		})
+		.catch(err => res.status(502).json({ message: err }))
 }
 
 module.exports = {
