2d93814b0c9b99cf15a59244e2bcaa73c1881fc8
cmtran09 <cmtran@hotmail.co.uk>
2020-05-01 18:31:15 +0100

    bug fix finaly made

--- backend/controllers/text_translate.js
+++ backend/controllers/text_translate.js
@@ -1,40 +1,51 @@
-const axios = require("axios")
+const axios = require('axios')
+const moment = require('moment');
 
 function translateAndText(req, res) {
 
-	const key = "trnsl.1.1.20200430T193714Z.38528beeb497af85.ccc8582dffab7fb59f5232f80c022fe83f128e44"
-	const text = req.body.message
-	const lang = req.body.language
-	const recipientNumber = req.body.number
+  const language = req.body.language
+  const number = req.body.number
+  const text = req.body.message
 
-	const twilioNumber = "+13163134784"
+  //Yandex
+  const key = 'trnsl.1.1.20191202T095947Z.54175132da211fc1.7560318e65d14be6fcc297876ea8f7cb07e89154'
+
+  //Twilio
+	const twilioNum = "+13163134784"
 	const accountSid = "AC7ca5ae686cef0503c846ba87009871ae"
 	const authToken = "cd018fe2561d1653477db763def27dbf"
 
-	const client = require("twilio")(accountSid, authToken)
+  const client = require('twilio')(accountSid, authToken)
 
-	axios.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=en-${lang}`)
-		.then(translatedText => {
-			console.log("translatedText.data : ", translatedText.data)
-			client.lookups.phoneNumbers(number)
-				.fetch({ countryCode: "GB" })
-				.then(phone_number => console.log(phone_number.phoneNumber))
-				.catch(err => res.status(400).json({ message: err }))
+  //get translation from yandex
+	axios.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${text}&lang=en-${language}`)
+    .then(translatedMessage => {
+      //ask twilio to check number 
+      console.log(translatedMessage)
+      client.lookups.phoneNumbers( number )
+        .fetch({ countryCode: 'GB' })
+        .then(phone_number => console.log(phone_number.phoneNumber))
+        .catch(err => res.status(400).json({ message: err }))
 
-			client.messages
-				.create({
-					body: translatedText.data.text[0],
-					from: twilioNumber,
-					to: `${recipientNumber}`
-				})
-				.then(message => {
-					console.log(message.sid)
-					if (message.sid) return res.status(200).json({ message: "Translated message sent!" })
-				})
-		})
-		.catch(err => res.status(502).json({ message: err }))
+      //ask Twilio to send sms
+      client.messages
+        .create({
+          body: translatedMessage.data.text[0],
+          from: twilioNum,
+          to: `${number}`
+        })
+        .then((message) => {
+          console.log(message.sid)
+          if (message.sid) return res.status(200).json({ 
+						message_status: 'Success Translated message sent!', 
+						sent_message: message.body,
+						time_sent: moment().format('MMMM Do YYYY, h:mm:ss a'),
+					})
+        })
+    })
+    .catch(err => console.log(err))
 }
 
 module.exports = {
-	translateAndText
+  translateAndText
 }
